89a4cf808e3f6d0cd8ec8640b7a6e01a4ed22de4
BianchiniLorenz <lorenzobianchini828@gmail.com>
2023-05-16 16:38:07 +0200

    prefabs

--- Assets/GameData/FallingPlatform.cs
+++ Assets/GameData/FallingPlatform.cs
@@ -6,8 +6,39 @@ public class FallingPlatform : MonoBehaviour
 {
     [SerializeField] float TimeBeforeFalling = 3;
     [SerializeField] float Countdown = 0;
+    [SerializeField] float Countdown1 = 0;
     [SerializeField] bool IsFalling = false;
+    [SerializeField] float TimeToRespawn = 3;
     [SerializeField] GameObject Platform;
+    Vector3 RespawnPos;
+
+    void Start()
+    {
+        RespawnPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (IsFalling)
+        {
+            //float counter = 0;
+            //counter += Time.deltaTime;
+            Countdown1 += Time.deltaTime;
+            if(Countdown1 >= TimeToRespawn)
+            {
+                Platform.transform.position = RespawnPos; 
+                transform.position = RespawnPos;
+                Platform.TryGetComponent(out BoxCollider collider);
+                collider.enabled = true;
+                Platform.TryGetComponent(out Rigidbody rb);
+                rb.useGravity = false;
+                rb.constraints = RigidbodyConstraints.FreezePosition | RigidbodyConstraints.FreezeRotation;
+                IsFalling = false;
+                Countdown1 = 0;
+            }
+        }
+    }
 
     void OnTriggerStay(Collider other)
     {
@@ -22,8 +53,10 @@ public class FallingPlatform : MonoBehaviour
                 Platform.TryGetComponent(out Rigidbody rb);
                 rb.useGravity = true;
                 rb.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezeRotation;
-                //other.TryGetComponent(out Rigidbody playerRB);
-                //playerRB.AddForce(0, -0.1f, 0);
+                other.TryGetComponent(out Rigidbody playerRB);
+                playerRB.AddForce(0, -0.1f, 0);
+                Countdown = 0;
+
             }
         }
     }
